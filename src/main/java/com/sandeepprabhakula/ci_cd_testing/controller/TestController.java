13c73a953b517175f3c25fb7639c54824371da55
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2026-02-25 21:04:17 +0530

    created a new endpoint

--- src/main/java/com/sandeepprabhakula/ci_cd_testing/controller/TestController.java
+++ src/main/java/com/sandeepprabhakula/ci_cd_testing/controller/TestController.java
@@ -10,4 +10,9 @@ public class TestController {
     public String getHello(){
         return "Helo world";
     }
+
+    @GetMapping("/getData")
+    public String getData(){
+        return "Data";
+    }
 }
