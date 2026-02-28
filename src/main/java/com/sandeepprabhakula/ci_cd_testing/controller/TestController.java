af3267b05937a7cd3b0e5622f0dd596a139dd87a
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2026-03-01 01:20:49 +0530

    added request mapping

--- src/main/java/com/sandeepprabhakula/ci_cd_testing/controller/TestController.java
+++ src/main/java/com/sandeepprabhakula/ci_cd_testing/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.sandeepprabhakula.ci_cd_testing.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class TestController {
 
     @GetMapping("/hello")
